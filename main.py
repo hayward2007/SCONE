28ed1d6daa93720ed00f203f43bb62c7abe19146
hayward2007 <hayward2007@naver.com>
2024-05-23 08:30:30 +0900

    feat: key input threading

--- main.py
+++ main.py
@@ -1,8 +1,48 @@
+import threading;
+import queue;
+import time;
+from getkey import getkey;
+
+
+def onKeyInput(keyQueue):
+    print('Ready for key input:')
+    while (True):
+        key = getkey()
+        keyQueue.put(key)
+
+
+
 if __name__ == "__main__" :
-    print("hello")
+    EXIT_COMMAND = 'q';
+
+    # print("hello")
+
+    keyQueue = queue.Queue()
+
+    keyThread = threading.Thread(target=onKeyInput, args=(keyQueue,))
+    keyThread.daemon = True 
+    keyThread.start()
+
+    while (True):
+        if (keyQueue.qsize() > 0):
+            key = keyQueue.get()
+            print("key = {}".format(key))
+
+            if (key == EXIT_COMMAND):
+                print("Exiting serial terminal.")
+                break            
+
+        time.sleep(0.01) 
+    print("End.")
+
+
+
+
+
     # while(True) :
     #     try :
     #         print("hasdf");
+
     #     except Exception as error :
     #         print(error);
     #         print("[SYSTEM] Executing program");
