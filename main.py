8177d16f669e841805405186bce2d2c89dfb6d35
hayward2007 <hayward2007@naver.com>
2024-05-23 09:51:40 +0900

    feat: print help by pressing h

--- main.py
+++ main.py
@@ -4,27 +4,42 @@ import time;
 import cv2;
 from getkey import getkey;
 
+# __CHANGE = 'f';
+# __INFO = 'i';
+# __HELP = 'h';
+__EXIT = 'q';
 
-def onKeyInput(keyQueue):
+
+def onKeyInput(keyQueue) :
     print('Ready for key input:')
     while (True):
         key = getkey()
         keyQueue.put(key)
 
+def printHelp() :
+    print("[SYSTEM] COMMANDS :");
+    print("\th : print commands, show this list");
+    print("\ti : print information & status");
+    print("\tf : change stance");
+    print("\t\tex) drive stance -> walk stance")
+
+
 if __name__ == "__main__" :
-    EXIT_COMMAND = 'q';
 
     keyQueue = queue.Queue()
     keyThread = threading.Thread(target=onKeyInput, args=(keyQueue,))
     keyThread.daemon = True 
     keyThread.start()
 
+    print("[SYSYEM] SCONE Activated");
+    print("[SYSYEM] Ready for key input");
     while (True):
         if (keyQueue.qsize() > 0):
             key = keyQueue.get()
-            print("key = {}".format(key))
-
-            if (key == EXIT_COMMAND):
+            # print("key = {}".format(key))
+            if (key == 'h') :
+                printHelp();
+            elif (key == 'q'):
                 print("Exiting serial terminal.")
                 break
 
